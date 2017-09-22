0ed13423e4f5df3d23eb27fa1943249a9c50baa5
Le Hoang Phuong <sigalaxy07@gmail.com>
2017-09-22 15:51:38 +0700

    add navigation, done change orther screens

---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,26 @@
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from '../screens/AuthScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import MapScreen from '../screens/MapScreen';
+import DeckScreen from '../screens/DeckScreen';
+import ReviewScreen from '../screens/ReviewScreen';
+import SettingScreen from '../screens/SettingScreen';
+
+const MainNavigator = TabNavigator({
+    welcome: { screen: WelcomeScreen },
+    auth: { screen: AuthScreen },
+    main: {
+        screen: TabNavigator({
+            map: { screen: MapScreen },
+            deck: { screen: DeckScreen },
+            review:{
+                screen: StackNavigator({
+                    review: {screen: ReviewScreen},
+                    setting: { screen: SettingScreen } 
+                })
+            }
+        })
+    }
+});
+export default MainNavigator;
--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  Platform
 } from 'react-native';
 
 export default class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Review Jobs",
+      headerRight:
+      (<TouchableOpacity onPress={() => { navigation.navigate('setting') }}>
+        <Text>Go Right</Text>
+      </TouchableOpacity>),
+      style:{
+        marginTop: Platform.OS === 'android'? 24:0
+      }
+    };
+  };
   render() {
     return (
       <View >
-       <Text>ReviewScreen</Text>
-       <Text>ReviewScreen</Text>
-       <Text>ReviewScreen</Text>
-       <Text>ReviewScreen</Text>
-       <Text>ReviewScreen</Text>
-       <Text>ReviewScreen</Text>
-       <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
       </View>
     );
   }
